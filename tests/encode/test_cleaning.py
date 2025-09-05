2e5da11a93ec58f968bb46f173e9100883673fb3
madagiurgiu25 <giurgiu.mada@gmail.com>
2025-09-05 11:43:34 +0200

    tests cleaning

--- tests/encode/test_cleaning.py
+++ tests/encode/test_cleaning.py
@@ -81,27 +81,63 @@ class TestMultiGraph(unittest.TestCase):
 		self.assertTrue(np.array_equal(m, m_aftercleaning, equal_nan=True))
   
 
-def test_clean_multi_vcf(self):
+def test_clean_multi_vcf():
 	""" Test multi-VCF file
 	"""
+	file_input = "tests/examples/vcfs/multivcf.vcf"
+	outdir = "tests/examples/vcfs/output_multivcf"
+	file_output = os.path.join(outdir,"clean.vcf")
+	os.makedirs("tests/examples/vcfs/output_multivcf", exist_ok=True)
+	encode.cleanvcf(file_input, 
+					file_output,
+					multi=True)
+	size1 = os.path.getsize(file_input)
+	size2 = os.path.getsize(file_output)
+	assert size1 == size2, f"File sizes differ: {file_input}={size1} bytes, {file_clean}={size2} bytes"
+	
 
-def test_read_and_clean_vcf(self):
-	os.makedirs("tests/examples/vcfs/output_sim1010_sniffles1_withartifacts", exist_ok=True)
-	encode.readvcf("tests/examples/vcfs/sim1010_sniffles1_withartifacts.vcf", 
-					"tests/examples/vcfs/output_sim1010_sniffles1_withartifacts", multi=False, svcaller=vp.SNIFFLES1)
+def test_read_and_clean_vcf(caplog):
+	file_input = "tests/examples/vcfs/sim1010_sniffles1_withartifacts.vcf"
+	outdir = "tests/examples/vcfs/output_sim1010_sniffles1_withartifacts"
+	file_clean = os.path.join(outdir, "clean.vcf")
+	file_clean_filtered = os.path.join(outdir, "clean_filtered.vcf")
+
+	with caplog.at_level(logging.DEBUG, logger="decoil.encode"):
+		os.makedirs(outdir, exist_ok=True)
+		encode.readvcf(file_input, 
+					   outdir, multi=False, svcaller=vp.SNIFFLES1)
+	
+ 	# no warnings
+	warning_occurrences = re.findall(r"WARNING", str(caplog.text), flags=re.MULTILINE)
+	count_warnings = len(warning_occurrences)
+	assert 0 == count_warnings
+ 
 	# test if clean.vcf and clean_filtered.vcf exists
+	assert os.path.exists(file_input), f"File does not exist: {file_input}"
+	assert os.path.exists(file_clean), f"File does not exist: {file_clean}"
 	
 	# test if the clean.vcf the same with input
+	size1 = os.path.getsize(file_input)
+	size2 = os.path.getsize(file_clean)
+	assert size1 == size2, f"File sizes differ: {file_input}={size1} bytes, {file_clean}={size2} bytes"
 	
 	# test if the clean_filtered.vcf removed specific columns
+	expected_lines = 234
 
-# pytest injects it
+	with open(file_clean_filtered, "r") as f:
+		actual_lines = sum(1 for _ in f)
+
+	assert actual_lines == expected_lines, f"Expected {expected_lines} lines, got {actual_lines}"
+ 
+
+# pytest injects caplog
 def test_clean_corrupted_vcf_warning(caplog):
 	with caplog.at_level(logging.DEBUG, logger="decoil.encode"):
 		os.makedirs("tests/examples/vcfs/output_sim1010_sniffles1_corrupted", exist_ok=True)
 		encode.cleanvcf("tests/examples/vcfs/sim1010_sniffles1_corrupted.vcf", 
 						os.path.join("tests/examples/vcfs/output_sim1010_sniffles1_corrupted","clean.vcf"),
 						multi=False)
+	# find two corrupted lines in the VCF
 	warning_occurrences = re.findall(r"WARNING", str(caplog.text), flags=re.MULTILINE)
 	count_warnings = len(warning_occurrences)
 	assert 2 == count_warnings
