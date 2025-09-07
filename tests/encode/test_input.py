74c3ac1155d3bf65c323d10736c167757c141596
madagiurgiu25 <giurgiu.mada@gmail.com>
2025-09-07 19:37:22 +0200

    fix tests

--- tests/encode/test_input.py
+++ tests/encode/test_input.py
@@ -17,16 +17,18 @@ class TestInput(unittest.TestCase):
         """Test correct format for sniffles2"""
         try:
             svinfo, collection_breakpoints, _ = parsevcf("tests/examples/ecdna1/sniffles2.vcf",False,vp.SNIFFLES2)
+            print(svinfo)
+            print(collection_breakpoints)
         except Exception as e:
             self.fail(f"Unexpected exception raised: {e}")
         # Optionally, check that the result is correct
-        # svinfo[chr2_15585355] [('Sniffles2.BND.2S1', 'chr3', '11049996', 'BND', 47, 88, '0/1', '+-'), ('Sniffles2.BND.3S1', 'chr3', '11056099', 'BND', 55, 80, '0/1', '-+')]
-        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][1][2], '11049996')
-        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][2][1], 'chr3')
-        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][2][7], '-+')
+        # svinfo[chr2_15585355] chr2@15585355': [('Sniffles2.BND.2S1', 'chr3', '11049996', 'BND', 47, 88, '0/1', '++'), ('Sniffles2.BND.3S1', 'chr3', '11056099', 'BND', 55, 80, '0/1', '-+')]
+        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][0][2], '11049996')
+        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][1][1], 'chr3')
+        self.assertEqual(svinfo["chr2"+SEPARATOR+"15585355"][1][7], '-+')
         
          # collection_breakpoints: defaultdict(<class 'list'>, {'chr2': [15585355, 15585355, 15633375, 16521051, 15585355], 'chr3': [11049996, 11056099, 10981201], 'chr12': [68807720, 68970909]})
-        self.assertEqual(collection_breakpoints["chr2"], [15585355, 15585355, 15585355, 15633375, 16521051, 15585355])
+        self.assertEqual(collection_breakpoints["chr2"], [15585355, 15585355, 15633375, 16521051, 15585355])
         
     def test_header_sniffles1(self):
         """Test correct format for sniffles1"""
